a52b2a3770ff84ac53e2eab7b43478335542d5ba
thara <tpshadinijk@gmail.com>
2023-06-15 23:00:57 +0530

    update test file and config.js

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,5 +1,7 @@
 describe('template spec', () => {
     it('Load web site visit weligama', () => {
       cy.visit('https://www.visitweligama.com/')
+      cy.get('#logo > h1 > a > #Header1_headerimg').click
+      //cy.contains(' How to get here - Weligama?')
     })
   })
